7ed11a69e8692c2435db0f8650636910256a2ec4
makdinoven <makdinoven@mail.ru>
2024-09-18 15:38:37 +0300

    add all presets.py

--- services/character-service/app/presets.py
+++ services/character-service/app/presets.py
@@ -1,42 +1,213 @@
 # Шаблоны атрибутов для подрас
 SUBRACE_ATTRIBUTES = {
-    1: {
+    1: { #Человек/Норд
         "strength": 20,
         "agility": 20,
         "intelligence": 10,
         "endurance": 10,
+
         "health": 10,
         "energy": 10,
         "mana": 0,
         "stamina": 10,
+
         "charisma": 0,
         "luck": 10
     },
-    2: {
-        "strength": 15,
+    2: { #Человек/ Ост
+        "strength": 10,
+        "agility": 30,
+        "intelligence": 20,
+        "endurance": 0,
+
+        "health": 5,
+        "energy": 5,
+        "mana": 5,
+        "stamina": 20,
+
+        "charisma": 5,
+        "luck": 10
+    },
+    3: { #Человек/Ориентал
+        "strength": 10,
+        "agility": 20,
+        "intelligence": 10,
+        "endurance": 20,
+
+        "health": 0,
+        "energy": 10,
+        "mana": 0,
+        "stamina": 10,
+
+        "charisma": 0,
+        "luck": 30
+    },
+    4: {  # Эльф/Малах
+        "strength": 5,
         "agility": 15,
         "intelligence": 20,
         "endurance": 10,
-        "health": 20,
+
+        "health": 0,
+        "energy": 0,
+        "mana": 10,
+        "stamina": 10,
+
+        "charisma": 10,
+        "luck": 20
+    },
+    5: {  # Эльф - Лесной
+        "strength": 0,
+        "agility": 30,
+        "intelligence": 20,
+        "endurance": 0,
+
+        "health": 5,
+        "energy": 10,
+        "mana": 0,
+        "stamina": 5,
+
+        "charisma": 20,
+        "luck": 10
+    },
+    6: {  # Эльф - темный
+        "strength": 15,
+        "agility": 15,
+        "intelligence": 15,
+        "endurance": 15,
+
+        "health": 0,
         "energy": 10,
         "mana": 10,
-        "stamina": 15,
-        "charisma": 5,
+        "stamina": 5,
+
+        "charisma": 0,
         "luck": 15
     },
-    # Добавьте еще 13 подрас с их характеристиками
-    # Пример:
-    3: {
-        "strength": 10,
-        "agility": 25,
-        "intelligence": 5,
-        "endurance": 10,
-        "health": 15,
-        "energy": 15,
+    7: {  # Драконид/Равагарт
+        "strength": 30,
+        "agility": 0,
+        "intelligence": 0,
+        "endurance": 30,
+
+        "health": 20,
+        "energy": 10,
         "mana": 0,
         "stamina": 10,
+
+        "charisma": 0,
+        "luck": 0
+    },
+    8: {  # Драконид/Рорис
+        "strength": 10,
+        "agility": 30,
+        "intelligence": 10,
+        "endurance": 10,
+
+        "health": 10,
+        "energy": 5,
+        "mana": 5,
+        "stamina": 0,
+
         "charisma": 0,
         "luck": 20
     },
-    # Подрасы 4-15
+    9: {  # Дворф/Золотой
+        "strength": 20,
+        "agility": 0,
+        "intelligence": 20,
+        "endurance": 10,
+
+        "health": 20,
+        "energy": 20,
+        "mana": 5,
+        "stamina": 5,
+
+        "charisma": 0,
+        "luck": 0
+    },
+    10: {  # Демон/Левиаан
+        "strength": 30,
+        "agility": 20,
+        "intelligence": 0,
+        "endurance": 0,
+
+        "health": 10,
+        "energy": 10,
+        "mana": 0,
+        "stamina": 10,
+
+        "charisma": 0,
+        "luck": 20
+    },
+    11: {  # Демон/Альб
+        "strength": 0,
+        "agility": 20,
+        "intelligence": 40,
+        "endurance": 0,
+
+        "health": 0,
+        "energy": 20,
+        "mana": 20,
+        "stamina": 0,
+
+        "charisma": 0,
+        "luck": 0
+    },
+    12: {  # Бистмен/Зверолюд
+        "strength": 20,
+        "agility": 20,
+        "intelligence": 0,
+        "endurance": 20,
+
+        "health": 5,
+        "energy": 10,
+        "mana": 10,
+        "stamina": 5,
+
+        "charisma": 0,
+        "luck": 10
+    },
+    13: {  # Бистмен/Полукровка
+        "strength": 0,
+        "agility": 40,
+        "intelligence": 10,
+        "endurance": 10,
+
+        "health": 5,
+        "energy": 10,
+        "mana": 0,
+        "stamina": 5,
+
+        "charisma": 10,
+        "luck": 10
+    },
+    14: {  # Урук - урук
+        "strength": 40,
+        "agility": 10,
+        "intelligence": 0,
+        "endurance": 30,
+
+        "health": 10,
+        "energy": 5,
+        "mana": 0,
+        "stamina": 5,
+
+        "charisma": 0,
+        "luck": 0
+    },
+    15: {  # Урук/Темный урук
+        "strength": 30,
+        "agility": 20,
+        "intelligence": 0,
+        "endurance": 20,
+
+        "health": 10,
+        "energy": 10,
+        "mana": 0,
+        "stamina": 0,
+
+        "charisma": 0,
+        "luck": 10
+    },
 }
