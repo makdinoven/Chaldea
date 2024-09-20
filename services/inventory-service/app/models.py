780419192f83435a23366ae6cb95a6a871a30d79
makdinoven <makdinoven@mail.ru>
2024-09-20 18:55:09 +0300

    prepare inventory

--- services/inventory-service/app/models.py
+++ services/inventory-service/app/models.py
@@ -1,15 +1,45 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Enum, Boolean, DECIMAL, Text, ForeignKey
+from sqlalchemy.orm import relationship
+
 from database import Base
 
 # Определяем модель для хранения инвентаря персонажа
 class CharacterInventory(Base):
     __tablename__ = "character_inventory"
 
-    id = Column(Integer, primary_key=True, index=True)  # Первичный ключ
-    character_id = Column(Integer, unique=True, index=True)  # ID персонажа
-    item_1 = Column(String(100), default="Basic Sword")  # Первый предмет
-    item_2 = Column(String(100), default="Basic Shield")  # Второй предмет
-    item_3 = Column(String(100), default="Health Potion")  # Третий предмет
-    # Возможность добавления дополнительных предметов
-    item_4 = Column(String(100), nullable=True)  # Дополнительный предмет (можно расширить)
-    item_5 = Column(String(100), nullable=True)  # Дополнительный предмет (можно расширить)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    character_id = Column(Integer, ForeignKey('characters.id'), nullable=False)
+    item_id = Column(Integer, ForeignKey('items.id'), nullable=False)
+    slot_type = Column(Enum('bag', 'armor_slot', 'weapon_slot', 'accessory_slot'), nullable=False)
+    quantity = Column(Integer, default=1)
+
+    character = relationship("Character", back_populates="inventory_items")
+    item = relationship("Item")
+
+
+# Таблица слотов экипировки
+class EquipmentSlot(Base):
+    __tablename__ = 'equipment_slots'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    character_id = Column(Integer, ForeignKey('characters.id'), nullable=False)
+    slot_type = Column(Enum('head', 'chest', 'legs', 'feet', 'weapon', 'accessory'), nullable=False)
+    item_id = Column(Integer, ForeignKey('items.id'), nullable=True)
+
+    character = relationship("Character", back_populates="equipment_slots")
+    item = relationship("Item")
+
+
+class Items(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), unique=True, index=True)
+    item_type = Column(Enum ('axe','armor','flask'), nullable=True)
+    is_stackable = Column(Boolean, nullable=False, default=False)
+    max_stack_size = Column(Integer, default=1)
+    weight = Column(DECIMAL(5, 2), nullable=False)
+    description = Column(Text)
+    is_sellable = Column(Boolean, default=False)
+    price = Column(Integer, nullable=True)
+    is_rare = Column(Boolean, nullable=False, default=False)
--- services/inventory-service/app/schemas.py
+++ services/inventory-service/app/schemas.py
@@ -1,13 +1,21 @@
 from pydantic import BaseModel
+from typing import Optional, List
+
+# Схема для отображения предмета в инвентаре
+class ItemInInventory(BaseModel):
+    id: int
+    name: str
+    item_type: str
+    quantity: int
+
+    class Config:
+        orm_mode = True  # Включаем режим ORM для поддержки работы с моделями SQLAlchemy
+
 
 # Базовая схема для инвентаря персонажа
 class CharacterInventoryBase(BaseModel):
     character_id: int
-    item_1: str = "Basic Sword"
-    item_2: str = "Basic Shield"
-    item_3: str = "Health Potion"
-    item_4: str = None
-    item_5: str = None
+    items: List[ItemInInventory]  # Список предметов в инвентаре
 
 # Схема для создания инвентаря персонажа
 class CharacterInventoryCreate(CharacterInventoryBase):
