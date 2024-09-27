7966c9329ed152b7c689ecca96b50fd40a6df3fd
wishes <galygin99@gmail.com>
2024-09-27 04:11:26 +0300

    Update SubmitPage.jsx

--- services/frontend/app-chaldea/src/components/CreateCharacterPage/SubmitPage/SubmitPage.jsx
+++ services/frontend/app-chaldea/src/components/CreateCharacterPage/SubmitPage/SubmitPage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 
 import FormButton from '../../CommonComponents/BlueGradientButton/BlueGradientButton';
 import CharacterInfo from './CharacterInfo/CharacterInfo';
@@ -32,6 +33,30 @@ export default function SubmitPage({
     { title: 'Внешность', text: biography.appearance },
   ];
 
+  const sendPhoto = () => {
+    const file = fileInputRef.current.files[0];
+    if (!file) {
+      console.log('Файл не выбран');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('avatar', file); // 'avatar' — это имя поля, которое будет на сервере.
+
+    axios
+      .post('/upload-photo', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data', // Указывает, что передаём данные формы
+        },
+      })
+      .then((response) => {
+        console.log('Фото загружено успешно:', response.data);
+      })
+      .catch((error) => {
+        console.error('Ошибка загрузки фото:', error);
+      });
+  };
+
   const characterItemsSmall = [
     { text: `${selectedRace} - ${selectedSubrace}` },
     { text: selectedClass },
@@ -46,6 +71,7 @@ export default function SubmitPage({
       <div className={styles.submit_container}>
         <div className={styles.left}>
           <div
+            // style={{ backgroundImage: url('../../../assets/menu2.png') }}
             className={styles.custom_file_upload}
             onClick={handlePhotoInputClick}
           >
@@ -54,13 +80,13 @@ export default function SubmitPage({
             </div>
             <label htmlFor='fileInput'>Сменить аватар</label>
             <input
-              style={{ display: 'none' }}
               ref={fileInputRef}
               type='file'
               id='fileInput'
               accept='image/*'
             />
           </div>
+          <button onClick={sendPhoto}>Отправить фото</button>
 
           <div className={styles.smallInfo_container}>
             {characterItemsSmall.map((item, index) => (
