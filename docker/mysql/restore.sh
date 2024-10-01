399912e170074dea3bd0d089d7f5c23296cbc39a
makdinoven <makdinoven@mail.ru>
2024-10-02 02:41:01 +0300

    auto upload backup

--- docker/mysql/restore.sh
+++ docker/mysql/restore.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-BACKUP_DIR="/docker/mysql/backups"
+echo "Скрипт восстановления запущен." >> /tmp/restore_log.txt
+
+BACKUP_DIR="/backups"
 LATEST_BACKUP=$(ls -t $BACKUP_DIR/*.sql | head -n 1)
 
 if [ -f "$LATEST_BACKUP" ]; then
-    echo "Восстанавливаю базу данных из бекапа: $LATEST_BACKUP"
-    mysql -u root -p$MYSQL_ROOT_PASSWORD < "$LATEST_BACKUP"
+    echo "Восстанавливаю базу данных из бекапа: $LATEST_BACKUP" >> /tmp/restore_log.txt
+
+    # Удаляем старую базу данных и создаем новую
+    mysql -u root -p$MYSQL_ROOT_PASSWORD -e "DROP DATABASE IF EXISTS $MYSQL_DATABASE; CREATE DATABASE $MYSQL_DATABASE;" 2>> /tmp/restore_log.txt
+
+    # Восстанавливаем базу данных из бекапа и записываем ошибки в лог
+    mysql -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < "$LATEST_BACKUP" 2>> /tmp/restore_log.txt
+
+    echo "Восстановление завершено." >> /tmp/restore_log.txt
 else
-    echo "Бекап не найден, пропускаю восстановление."
+    echo "Бекап не найден, пропускаю восстановление." >> /tmp/restore_log.txt
 fi
+
+# Запуск MySQL после восстановления
+exec docker-entrypoint.sh mysqld
