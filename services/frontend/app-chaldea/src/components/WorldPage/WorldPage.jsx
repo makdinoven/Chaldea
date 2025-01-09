b9c5f16723afd8f802cabaf2915c86cc154c2261
wishes <galygin99@gmail.com>
2025-01-09 22:17:04 +0300

    skdfjskf

--- services/frontend/app-chaldea/src/components/WorldPage/WorldPage.jsx
+++ services/frontend/app-chaldea/src/components/WorldPage/WorldPage.jsx
@@ -1,9 +1,28 @@
 import s from './WorldPage.module.scss'
 import axios from "axios";
+import {useEffect} from "react";
+import Map from "./Map/Map.jsx";
 
 export default function WorldPage() {
 
-    axios.get()
-    return (<h1>World page</h1>)
+    useEffect(() => {
+        axios
+            .get('http://localhost:8006/locations/locations/lookup', {
+                headers: {
+                    Accept: 'application/json',
+                },
+            })
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => console.log(error));
+    }, []);
+
+
+    return (
+        <>
+            <h1>World page</h1>
+            <Map />
+            </>)
 
 }
