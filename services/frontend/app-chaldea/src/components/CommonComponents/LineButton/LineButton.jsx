296fa07c7b516d585da6511b387ae095ca69ef87
Alexey <galygin99@gmail.com>
2025-04-09 00:46:21 +0300

    map tooltip desc, navigate to region

--- services/frontend/app-chaldea/src/components/CommonComponents/LineButton/LineButton.jsx
+++ services/frontend/app-chaldea/src/components/CommonComponents/LineButton/LineButton.jsx
@@ -1,6 +1,6 @@
 import s from './LineButton.module.scss';
 import {Link} from "react-router-dom";
 
-export default function LineButton({text, link}) {
-    return (<Link className={s.button} to={link}>{text}</Link>)
+export default function LineButton({text, link, onClick}) {
+    return (<Link onClick={onClick} className={s.button} to={link}>{text}</Link>)
 }
