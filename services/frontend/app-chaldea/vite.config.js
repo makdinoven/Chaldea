62866cef20406647cdce1e5e897e6679af6463d6
makdinoven <makdinoven@mail.ru>
2025-04-18 02:20:49 +0300

    Fix allowed host

--- services/frontend/app-chaldea/vite.config.js
+++ services/frontend/app-chaldea/vite.config.js
@@ -1,13 +1,15 @@
-// vite.config.js
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: true,
-        watch: {
-            usePolling: true
-        }
-    }
+  plugins: [react()],
+  server: {
+    host: true,               // слушать на всех интерфейсах
+    watch: {
+      usePolling: true,       // если вам нужен polling
+    },
+    allowedHosts: 'all'
+    // или, чтобы разрешить _все_ хосты (менее безопасно):
+    // allowedHosts: 'all',
+  },
 });
