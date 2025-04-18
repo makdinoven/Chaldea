6c062e762acc07b7e00dc041bb0fd0ddf67a1aa4
Alexey <galygin99@gmail.com>
2025-04-18 19:53:47 +0300

    header fix, user context remove

--- services/frontend/app-chaldea/src/main.jsx
+++ services/frontend/app-chaldea/src/main.jsx
@@ -1,23 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App/App.jsx';
-import { UserProvider } from '../src/hooks/UserContext.jsx';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
+import "./index.css";
 
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <Provider store={store}>
-          <UserProvider>
-              <App />
-          </UserProvider>
-      </Provider>
-  </React.StrictMode>
-
-  // <UserProvider>
-  //   <App />
-  // </UserProvider>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
