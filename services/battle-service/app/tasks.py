621d5757b0a979e00d02a8cb7cb4d4f8b94f84a9
makdinoven <makdinoven@mail.ru>
2025-04-28 01:46:48 +0300

    v 1.0 for battle-service

--- services/battle-service/app/tasks.py
+++ services/battle-service/app/tasks.py
@@ -2,13 +2,13 @@ import asyncio, contextlib
 import os
 from datetime import datetime
 from celery import Celery
-from mongo_client import get_db
+from mongo_client import get_mongo_db
 
 celery_app = Celery("battle_tasks", broker=os.getenv("CELERY_BROKER_URL", "redis://redis:6379/1"))
 
 @celery_app.task
 def save_log(battle_id: int, turn_number: int, events: list[dict]):
-    db = get_db()                         # AsyncIOMotorDatabase
+    db = get_mongo_db()                         # AsyncIOMotorDatabase
     async def _insert():
         await db.battle_logs.insert_one({
             "battle_id":  battle_id,
